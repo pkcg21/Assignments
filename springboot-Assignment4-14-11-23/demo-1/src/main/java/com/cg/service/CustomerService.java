38a19e27154effe5d49916237be5eda1bd5c1005
pkcg21 <149366132+pkcg21@users.noreply.github.com>
2023-11-15 17:43:45 +0530

    Add files via upload

---
+++ springboot-Assignment4-14-11-23/demo-1/src/main/java/com/cg/service/CustomerService.java
@@ -0,0 +1,38 @@
+package com.cg.service;
+
+import java.util.List;
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.entity.Customer;
+
+import com.cg.repository.CustomerRepository;
+
+
+@Service
+@org.springframework.transaction.annotation.Transactional
+public class CustomerService {
+
+	@Autowired
+	 private CustomerRepository repo;
+	public List<Customer> listAll(){
+		List<Customer> details=repo.findAll();
+		System.out.println(details);
+		return details;
+		
+	}
+	public void save(Customer customer) {
+		repo.save(customer);
+	}
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+	public Customer get(Long id) {
+		return repo.findById(id).get();
+	}
+
+}
